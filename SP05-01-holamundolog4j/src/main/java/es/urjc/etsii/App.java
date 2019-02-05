f6abe3d6134a0f857802b323c2c143ca689decde
J. Manuel Colmenar <josemanuel.colmenar@urjc.es>
2019-02-05 16:12:45 +0100

    SP05 - Hola Mundo Maven Log4j

---
+++ SP05-01-holamundolog4j/src/main/java/es/urjc/etsii/App.java
@@ -0,0 +1,15 @@
+package es.urjc.etsii;
+
+import org.apache.logging.log4j.LogManager;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        LogManager.getLogger(App.class).info("Hola mundo.");
+    }
+}
